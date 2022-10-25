805a367aae4495e9c8afa9b4b05aecfedfca39a4
leighannharris <eleighannharris@live.seminolestate.edu>
2022-10-24 22:03:52 -0400

    Changes for Reach Router

--- api/scripts/init.mongo.js
+++ api/scripts/init.mongo.js
@@ -2,19 +2,36 @@
 /* global db print */
 /* eslint no-restricted-globals: "off" */
 
-
 db.issues.remove({});
 
 const issuesDB = [
   {
-    id: 1, status: 'New', owner: 'Ravan', effort: 5,
-    created: new Date('2018-08-15'), due: undefined,
-    title: 'Error in console when clicking Add'
+    id: 1,
+    status: 'New',
+    owner: 'Ravan',
+    effort: 5,
+    created: new Date('2018-08-15'),
+    due: undefined,
+    title: 'Error in console when clicking Add',
+    description: 'Steps to recreate the problem:'
+      + '\n1. Refresh the browser.'
+      + '\n2. Select "New" in the filter'
+      + '\n3. Refresh the browser again. Note the warning in the console:'
+      + '\n    Warning: Hash history cannot PUSH the same path; a new entry'
+      + '      will not be added to the history stack'
+      + '\n4. Click on Add.'
+      + '\n5. There is an error in console, and add doesn\'t work.',
   },
   {
-    id: 2, status: 'Assigned', owner: 'Eddie', effort: 14,
-    created: new Date('2018-08-16'), due: new Date('2018-08-30'),
-    title: 'Missing bottom border on panel'
+    id: 2,
+    status: 'Assigned',
+    owner: 'Eddie',
+    effort: 14,
+    created: new Date('2018-08-16'),
+    due: new Date('2018-08-30'),
+    title: 'Missing bottom border on panel',
+    description: 'There needs to be a border in the bottom in the panel'
+      + ' that appears when clicking on Add',
   },
 ];
 
--- ui/src/IssueTable.jsx
+++ ui/src/IssueTable.jsx
@@ -1,7 +1,9 @@
 
 import React from 'react';
+import { Link, NavLink, withRouter } from 'react-router-dom';
 
-function IssueRow({ issue }) {
+const IssueRow = withRouter(({ issue, location: { search } }) => {
+  const selectLocation = { pathname: `/issues/${issue.id}`, search };
   return (
     <tr>
       <td>{issue.id}</td>
@@ -11,9 +13,15 @@ function IssueRow({ issue }) {
       <td>{issue.effort}</td>
       <td>{issue.due ? issue.due.toDateString() : ' '}</td>
       <td>{issue.title}</td>
+      <td>
+        <Link to={`/edit/${issue.id}`}>Edit</Link>
+        {' | '}
+        <NavLink to={selectLocation}>Select</NavLink>
+      </td>
+
     </tr>
   );
-}
+});
 
 export default function IssueTable({ issues }) {
   const issueRows = issues.map(issue =>
@@ -31,6 +39,7 @@ export default function IssueTable({ issues }) {
           <th>Effort</th>
           <th>Due Date</th>
           <th>Title</th>
+          <th>Action</th>
         </tr>
       </thead>
       <tbody>
